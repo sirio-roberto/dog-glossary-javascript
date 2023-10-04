f99e5333892b80d1fea09ae8c5d8b8d8f7b78adf
Sirio <sirioroberto1@hotmail.com>
2023-10-04 17:24:42 -0300

    Simplified code with innerHTML, so we can skip some checks. Also, added the try/catch handling

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 const contentDiv = document.getElementById("content");
 const showDogBtn = document.getElementById("button-random-dog");
+const url = "https://dog.ceo/api/breeds/image/random";
 
-showDogBtn.addEventListener("click", async () => {
-  const response = await fetch("https://dog.ceo/api/breeds/image/random");
-  if (response.ok) {
+showDogBtn.addEventListener("click", fetchImage);
+
+
+async function fetchImage() {
+  try {
+    const response = await fetch(url);
     const data = await response.json();
-    if (data.status === "success") {
-      const imageUrl = data.message;
-      let imgElement = document.getElementById("dog-img");
-      if (imgElement === null) {
-        imgElement = document.createElement("img");
-        imgElement.setAttribute("alt", "dog image");
-        imgElement.setAttribute("id", "dog-img");
-        contentDiv.appendChild(imgElement);
-      }
-      imgElement.setAttribute("src", imageUrl);
-    }
+    const imageUrl = data.message;
+    let imgElement = document.createElement("img");
+    imgElement.alt = "dog image";
+    imgElement.id = "dog-img";
+    imgElement.src = imageUrl;
+    contentDiv.innerHTML = imgElement.outerHTML;
+  } catch (error) {
+    console.log("error when trying to fetch image");
   }
-})
+}
