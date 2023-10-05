9d786f34b372bd99e72a6956e3cb41385b43bc00
Sirio <sirioroberto1@hotmail.com>
2023-10-04 23:54:39 -0300

    Created form input and button to let users look for random images of specific breeds. Error message is shown if breed does not exist

--- src/main.js
+++ src/main.js
@@ -1,21 +1,33 @@
 const contentDiv = document.getElementById("content");
 const showDogBtn = document.getElementById("button-random-dog");
-const url = "https://dog.ceo/api/breeds/image/random";
+const showBreedBtn = document.getElementById("button-show-breed");
+const inputBreed = document.getElementById("input-breed");
+const randomUrl = "https://dog.ceo/api/breeds/image/random";
 
-showDogBtn.addEventListener("click", fetchImage);
+showDogBtn.addEventListener("click", () => fetchImage(randomUrl));
+showBreedBtn.addEventListener("click", () => {
+  const breed = inputBreed.value.toLowerCase();
+  const breedRandomUrl = `https://dog.ceo/api/breed/${breed}/images/random`;
+  return fetchImage(breedRandomUrl);
+});
 
-
-async function fetchImage() {
+async function fetchImage(url) {
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    const imageUrl = data.message;
-    let imgElement = document.createElement("img");
-    imgElement.alt = "dog image";
-    imgElement.id = "dog-img";
-    imgElement.src = imageUrl;
-    contentDiv.innerHTML = imgElement.outerHTML;
+    if (response.status >= 400) {
+      let errorParagraph = document.createElement("p");
+      errorParagraph.innerText = "Breed not found!";
+      contentDiv.innerHTML = errorParagraph.outerHTML;
+    } else {
+      const data = await response.json();
+      const imageUrl = data.message;
+      let imgElement = document.createElement("img");
+      imgElement.alt = "dog image";
+      imgElement.id = "dog-img";
+      imgElement.src = imageUrl;
+      contentDiv.innerHTML = imgElement.outerHTML;
+    }
   } catch (error) {
-    console.log("error when trying to fetch image");
+    console.log(error);
   }
 }
