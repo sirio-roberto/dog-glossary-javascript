dedadd79d406255320fb4836cb058f8399a5b5a5
Sirio <sirioroberto1@hotmail.com>
2023-10-05 09:33:20 -0300

    Created new button to show a list of sub breeds

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const contentDiv = document.getElementById("content");
 const showDogBtn = document.getElementById("button-random-dog");
 const showBreedBtn = document.getElementById("button-show-breed");
+const showSubBreedBtn = document.getElementById("button-show-sub-breed");
 const inputBreed = document.getElementById("input-breed");
 const randomUrl = "https://dog.ceo/api/breeds/image/random";
 
@@ -10,6 +11,7 @@ showBreedBtn.addEventListener("click", () => {
   const breedRandomUrl = `https://dog.ceo/api/breed/${breed}/images/random`;
   return fetchImage(breedRandomUrl);
 });
+showSubBreedBtn.addEventListener("click", () => fetchSubBreedList());
 
 async function fetchImage(url) {
   try {
@@ -26,6 +28,37 @@ async function fetchImage(url) {
       imgElement.id = "dog-img";
       imgElement.src = imageUrl;
       contentDiv.innerHTML = imgElement.outerHTML;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function fetchSubBreedList(){
+  try {
+    const url = `https://dog.ceo/api/breed/${inputBreed.value.toLowerCase()}/list`;
+    const response = await fetch(url);
+    if (response.status >= 400) {
+      let errorParagraph = document.createElement("p");
+      errorParagraph.innerText = "Breed not found!";
+      contentDiv.innerHTML = errorParagraph.outerHTML;
+    } else {
+      const data = await response.json();
+      const breedArray = data.message;
+
+      if (breedArray.length === 0) {
+        let errorParagraph = document.createElement("p");
+        errorParagraph.innerText = "No sub-breeds found!";
+        contentDiv.innerHTML = errorParagraph.outerHTML;
+      } else {
+        const orderedList = document.createElement("ol");
+        breedArray.forEach(breed => {
+          const item = document.createElement("li");
+          item.innerText = breed;
+          orderedList.append(item);
+        });
+        contentDiv.innerHTML = orderedList.outerHTML;
+      }
     }
   } catch (error) {
     console.log(error);
